661f20c7cbda8ca783bd4d4abbab2c72f1472694
Trevor Smith <tsmith3@entertainment.com>
2015-04-20 13:53:28 -0400

    initial commit of install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,41 @@
 #
 class puppetdb2_rundeck::install {
 
-  package { $::puppetdb2_rundeck::package_name:
-    ensure => present,
+  package { 'rubygem-sinatra':
+    ensure  => present,
   }
+
+  class { '::staging':
+    path  => '/tmp/staging',
+    owner => 'puppet',
+    group => 'puppet',
+  }
+
+  staging::file { 'puppetdb2-rundeck.tar.gz':
+    source => 'https://github.com/sirloper/puppetdb2-rundeck/archive/1.0.2.tar.gz',
+  }
+
+  staging::extract { 'puppetdb2-rundeck.tar.gz':
+    target  => $::puppetdb2_rundeck::apache_docroot,
+    creates => "${::puppetdb2_rundeck::apache_docroot}/puppetdb2_rundeck",
+    require => Staging::File['puppetdb2-rundeck.tar.gz'],
+  }
+
+  file { "${::puppetdb2_rundeck::apache_docroot}/puppetdb2_rundeck":
+    owner     => 'root',
+    group     => $::puppetdb2_rundeck::apache_group,
+    mode      => '0640',
+    recurse   => true,
+    subscribe => Staging::Extract[ 'puppetdb2-rundeck.tar.gz'],
+  }
+
+  file { $::puppetdb2_rundeck::apache_docroot:
+    ensure  => directory,
+    owner   => 'root',
+    group   => $::puppetdb2_rundeck::apache_group,
+    mode    => '0750',
+    require => Group[$::puppetdb2_rundeck::apache_group],
+  }
+
+
 }
