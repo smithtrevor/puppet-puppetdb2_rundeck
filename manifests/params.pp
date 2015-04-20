54933d81bd59eba675ae14dd226345d28f5f91aa
Trevor Smith <tsmith3@entertainment.com>
2015-04-20 13:56:46 -0400

    initial commit of params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,17 +4,37 @@
 # It sets variables according to platform.
 #
 class puppetdb2_rundeck::params {
+  $package_source = 'https://github.com/sirloper/puppetdb2-rundeck/archive/1.0.2.tar.gz'
+
+  $apache_serveradmin = "root@${::fqdn}"
+
+  $ssl_params_hash = {
+    'ssl_cert'             => getvar(::apache::default_ssl_cert),
+    'ssl_key'              => getvar(::apache::default_ssl_key),
+    'ssl_chain'            => getvar(::apache::default_ssl_chain),
+    'ssl_ca'               => getvar(::apache::default_ssl_ca),
+    'ssl_crl_path'         => getvar(::apache::default_ssl_crl_path),
+    'ssl_crl'              => getvar(::apache::default_ssl_crl),
+    'ssl_certs_dir'        => getvar(::apache::params::ssl_certs_dir),
+    'ssl_protocol'         => undef,
+    'ssl_cipher'           => undef,
+    'ssl_honorcipherorder' => undef,
+    'ssl_verify_client'    => undef,
+    'ssl_verify_depth'     => undef,
+    'ssl_options'          => undef,
+    'ssl_proxyengine'      => undef,
+  }
+
   case $::osfamily {
-    'Debian': {
-      $package_name = 'puppetdb2_rundeck'
-      $service_name = 'puppetdb2_rundeck'
-    }
     'RedHat', 'Amazon': {
-      $package_name = 'puppetdb2_rundeck'
-      $service_name = 'puppetdb2_rundeck'
+      $apache_docroot = '/var/www/html'
+      $apache_user = 'apache'
+      $apache_group = 'apache'
+      $apache_server_root = '/etc/httpd'
     }
     default: {
       fail("${::operatingsystem} not supported")
     }
   }
+
 }
