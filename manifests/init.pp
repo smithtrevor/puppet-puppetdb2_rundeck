65078013a41cb53070fed9dad95f21c8923dc9af
Trevor Smith <tsmith3@entertainment.com>
2015-04-20 16:09:36 -0400

    fix copy paste errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,14 +8,14 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class puppetdb2_rundeck (
-  $apache_docroot         = $::phpipam::params::apache_docroot,         # document root for apache this is the parent directory to the phpipam docroot
-  $apache_user            = $::phpipam::params::apache_user,            # user account under which the httpd processes run
-  $apache_group           = $::phpipam::params::apache_group,           # group account for the apache_user
-  $package_source         = $::phpipam::params::package_source,         # source url for the phpipam archive
+  $apache_docroot         = $::puppetdb2_rundeck::params::apache_docroot,         # document root for apache this is the parent directory to the puppetdb2_rundeck docroot
+  $apache_user            = $::puppetdb2_rundeck::params::apache_user,            # user account under which the httpd processes run
+  $apache_group           = $::puppetdb2_rundeck::params::apache_group,           # group account for the apache_user
+  $package_source         = $::puppetdb2_rundeck::params::package_source,         # source url for the puppetdb2_rundeck archive
   $manage_apache          = true,                                       # boolean flag for enabling this module to manage apache
   $manage_apache_vhost    = true,                                       # boolean flag for enabling this module to manage the virtual host
-  $apache_server_root     = $::phpipam::params::apache_server_root,     # the root directory for the httpd configuration files
-  $apache_serveradmin     = $::phpipam::params::apache_serveradmin,     # serveradmin value as set in the vhost configuration
+  $apache_server_root     = $::puppetdb2_rundeck::params::apache_server_root,     # the root directory for the httpd configuration files
+  $apache_serveradmin     = $::puppetdb2_rundeck::params::apache_serveradmin,     # serveradmin value as set in the vhost configuration
   $site_fqdn              = $::fqdn,                                    # fully qualified domain name for the web site
 ) inherits ::puppetdb2_rundeck::params {
 
